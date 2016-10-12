c4dd530ec4f2118bad6f564513670eb988710070
fawzy@outlook.com <fawzy.zayed@edigits.net>
2016-10-12 17:37:50 +0200

    add basic slide show and now working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,58 @@
-    var slideItem = '.slideItem';
-            var slideCount = slideItem().length;
-            var slideWidth = 100.0 / slideCount;
-            var slideIndex = 0;
-            
-            
-            slideItem.find("img").each(function(slideImg) {
-                var leftPercent = (slideWidth * slideImg) + "%";
-                $(this).css({"left":leftPercent});
-                $(this).css({width:(100 / slideCount) + "%"});
-            });
-            
-            
-            // Listen for click of prev button
-            $(".slideRight").click(function() {
-                console.log("prev button clicked");
-                slide(slideIndex - 1);
-            });
+sliderInt = 1;
+sliderNext = 2;
 
-            // Listen for click of next button
-            $(".slideLeft").click(function() {
-                console.log("next button clicked");
-                slide(slideIndex + 1);
-            });
-            
-            function slide(newSlideIndex) {   
-                if(newSlideIndex < 0 || newSlideIndex >= slideCount) return; 
-                var marginLeft = (newSlideIndex * (-100)) + "%";
-                slideItem.animate({"margin-left": marginLeft}, 400, function() {
-                    slideIndex = newSlideIndex
-                });
-            }
-            
+$(document).ready(function(){
+    $(".slideMap > .slideItem.1").fadeIn(300);
+    startSlider();
+});
+
+function startSlider(){
+    count= $(".slideMap > .slideItem").size();
+    loop = setInterval(function(){
+        if(sliderNext > count){
+            sliderNext = 1;
+            sliderInt = 1;
+        }
+        $(".slideMap > .slideItem").fadeOut(300);
+        $(".slideMap > .slideItem." + sliderNext).fadeIn(300);
+        sliderInt = sliderNext;
+        sliderNext = sliderNext +1;
+        
+    },3000);
+}
+
+function prev(){
+    newSlide = sliderInt - 1;
+    showSlide(newSlide);
+}
+function next(){
+    newSlide = sliderInt + 1;
+    showSlide(newSlide);
+}
+
+function stopLoop(){
+    window.clearInterval(loop)
+}
+
+function showSlide(Id){
+    stopLoop();
+    if(Id > count){
+        Id = 1;
+    }else if (Id < 1){
+        Id=count;
+    }
+    $(".slideMap > .slideItem").fadeOut(300);
+    $(".slideMap > .slideItem." + Id).fadeIn(300);
+    sliderInt = Id;
+    sliderNext = Id +1;
+    startSlider();
+}
+// stop slider when hover 
+$(".slideMap > .slideItem").hover(
+    function(){
+        stopLoop();
+    },
+    function(){
+        startSlider();
+    }
+);
